9e57eb13f6e8c8e373e64d8d518fb5a13e85969a
ybdt <65813455+ybdt@users.noreply.github.com>
2021-01-18 11:16:01 +0800

    Create zoomeye_ip_extract.py

---
+++ 2021_01_18_ZoomEye搜索结果ip提取工具/zoomeye_ip_extract.py
@@ -0,0 +1,32 @@
+import json
+import sys
+import platform
+
+def extract(file, count):
+    with open(file, "r") as f0:
+        json_content = f0.read();
+        dict_obj = json.loads(json_content);
+        #print( dict_obj.keys() );
+        #print( dict_obj["matches"][0].keys() );
+        with open("zoomeye_ip_output.txt", "w") as f1:
+            for i in range(count):
+                ip = dict_obj["matches"][i]["ip"];
+                print(ip);
+                if platform.system() == "Linux":
+                    f1.write(ip + "\n");
+                elif platform.system() == "Windows":
+                    f1.write(ip + "\r\n");
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: python3 zoomeye_ip_extract.py \"app:'飞致云堡垒机'_160_1610893510.json\" 160");#参数值有单引号，需用双引号扩起来，否则不能正确识别
+        exit();
+    else:
+        file = sys.argv[1];
+        #print(file);
+        count = int(sys.argv[2]);
+    
+    extract(file, count);
+    print("The output file is zoomeye_ip_output.txt");
+
+main();
